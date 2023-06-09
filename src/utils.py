9c1f519dcc458f757eb14f88113f6ec5dc2f3d70
pedro-schicchi <135450550+pedro-schicchi@users.noreply.github.com>
2023-06-09 16:02:39 -0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,7 @@ import os
 def delete_all_files(folder):
     for fn in os.listdir(folder):
         os.remove(os.path.join(folder,fn))
+        
+def check_for_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
